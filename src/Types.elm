0ca5e1a0208d4f7547e51fbe083eaa2ab82ae88e
4afs <g2x649@gmail.com>
2019-09-03 13:40:36 +0900

    define Card

--- src/Types.elm
+++ src/Types.elm
@@ -1,1 +1,30 @@
 module Types exposing (..)
+
+
+type alias Card =
+    { suit : Suit
+    , rank : Rank
+    }
+
+
+type Suit
+    = Spade
+    | Diamond
+    | Club
+    | Heart
+
+
+type Rank
+    = Ace
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
+    | Jack
+    | Queen
+    | King
