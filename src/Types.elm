44ddac144b3c06378683cb702706ca45b0c38735
4afs <g2x649@gmail.com>
2019-09-03 13:49:50 +0900

    define Players, Points, States and Judge

--- src/Types.elm
+++ src/Types.elm
@@ -28,3 +28,32 @@ type Rank
     | Jack
     | Queen
     | King
+
+
+type alias Player =
+    { hands : List Card
+    , points : Points
+    }
+
+
+type alias Dealer =
+    { hands : List Card
+    , points : Points
+    }
+
+
+type Points
+    = Bust
+    | Points Int
+
+
+type States
+    = Init
+    | Playing
+    | Finish Judge
+
+
+type Judge
+    = Win
+    | Draw
+    | Lose
