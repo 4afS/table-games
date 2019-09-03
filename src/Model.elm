997e55c08aaef6b23f18cd2db8c952dc99b675c2
Index197511 <hiro.saten@gmail.com>
2019-09-03 13:46:12 +0900

    define Model

--- src/Model.elm
+++ src/Model.elm
@@ -1,1 +1,10 @@
-module Model exposing (..)
+module Model exposing (Model)
+
+import Msg exposing (..)
+
+type alias Model =
+    { deck : List Card
+    , gameStatus : States
+    , dealer : Player
+    , player : Player
+    }
